5deec31231823f6744b4a3566ab003dedd03fad6
George Barrett <georgeaustinbarrett@gmail.com>
2023-05-05 15:57:05 +0100

    code tidy

--- spec/seeds_items.sql
+++ spec/seeds_items.sql
@@ -1,7 +1,6 @@
-TRUNCATE TABLE items RESTART IDENTITY CASCADE; -- replace with your own table name.
+TRUNCATE TABLE items RESTART IDENTITY CASCADE; 
 
--- Below this line there should only be `INSERT` statements.
--- Replace these statements with your own seed data.
+
 
 INSERT INTO items (name, unit_price, quantity) VALUES ('Cup', '4.99', 8);
 INSERT INTO items (name, unit_price, quantity) VALUES ('Lamp', '9.98', 6);
--- spec/seeds_orders.sql
+++ spec/seeds_orders.sql
@@ -1,7 +1,6 @@
-TRUNCATE TABLE orders RESTART IDENTITY CASCADE; -- replace with your own table name.
+TRUNCATE TABLE orders RESTART IDENTITY CASCADE; 
 
--- Below this line there should only be `INSERT` statements.
--- Replace these statements with your own seed data.
+
 
 INSERT INTO orders (customer_name, date, item_id) VALUES ('Nate', '2012-11-04', 1);
 INSERT INTO orders (customer_name, date, item_id) VALUES ('Will', '2012-07-09', 2);
